134bcdc593f9bb4bf0bc19afad0f32629383d979
patwalnik <32880451+patwalnik@users.noreply.github.com>
2020-07-23 07:05:52 +0530

    swagger ui added

---
+++ precache-manifest.212091fa2acff9e75d5ad576eb8dec3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f1167f2a9a9f607fcaa9c5e2fd47a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51bcc85f7f68a936acf6",
+    "url": "/static/css/2.bb78bf08.chunk.css"
+  },
+  {
+    "revision": "97eb74f73c30efe60f21",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "51bcc85f7f68a936acf6",
+    "url": "/static/js/2.0ec71f9a.chunk.js"
+  },
+  {
+    "revision": "3ad154199f148d9b0ca5e3d7052f4e6e",
+    "url": "/static/js/2.0ec71f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97eb74f73c30efe60f21",
+    "url": "/static/js/main.52f6485c.chunk.js"
+  },
+  {
+    "revision": "6f8a98b6ba9a43199a4e",
+    "url": "/static/js/runtime-main.94bafeed.js"
+  }
+]);
